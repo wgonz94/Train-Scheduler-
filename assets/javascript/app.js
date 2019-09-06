ed3be1d283ea5eb98923ca934ad68b4225074a4d
William Gonzales <wgonz94@yahoo.com>
2019-09-06 00:50:47 -0700

    added click event for train

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,7 @@ var firebaseConfig = {
     apiKey: "AIzaSyByM0nPbj0AAtXVHoM9FIQFx00sEdxz-gg",
     authDomain: "train-sched-ca76f.firebaseapp.com",
     databaseURL: "https://train-sched-ca76f.firebaseio.com",
-    projectId: "train-sched-ca76f",
     storageBucket: "",
-    messagingSenderId: "442034861594",
-    appId: "1:442034861594:web:4df95230c5982240b583a3"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -33,4 +30,6 @@ var firebaseConfig = {
       console.log(newTrain.destination)
       console.log(newTrain.time)
       console.log(newTrain.frequency)
+
+      alert("Added Train!!")
   })
