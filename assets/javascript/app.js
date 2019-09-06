43163e562a827eacdf70fea658b6559cdbcc4c60
William Gonzales <wgonz94@yahoo.com>
2019-09-05 23:43:43 -0700

    added firebase config to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyByM0nPbj0AAtXVHoM9FIQFx00sEdxz-gg",
+    authDomain: "train-sched-ca76f.firebaseapp.com",
+    databaseURL: "https://train-sched-ca76f.firebaseio.com",
+    projectId: "train-sched-ca76f",
+    storageBucket: "",
+    messagingSenderId: "442034861594",
+    appId: "1:442034861594:web:4df95230c5982240b583a3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
